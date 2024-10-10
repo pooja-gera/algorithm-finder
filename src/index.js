7a52cf828fb09916986146be8cee467bb92d2006
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2024-10-10 13:20:42 +0530

    fixes for full page signin

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+// Todo: implement this via be
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,5 +15,3 @@ root.render(
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-
